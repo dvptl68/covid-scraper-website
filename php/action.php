a49e307fa3a067009cdc65519c79edfc64e3214b
Dev Patel <dvptl68@gmail.com>
2020-07-02 08:16:07 -0400

    Add request method checking

--- php/action.php
+++ php/action.php
@@ -1,6 +1,14 @@
 <?php
 
+// Terminate script if request method is not post
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') exit();
+
+// Read configuration data from file
 $myfile = fopen("config.json", "r");
 $config = json_decode(fread($myfile,filesize("config.json")), true);
+fclose($myfile);
 
-// header('Location: ../success.html');
+
+
+// Redirect to success page
+header('Location: ../success.html');
