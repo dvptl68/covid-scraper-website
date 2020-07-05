b83117f9427c80439316efc88259e5c58d45a067
Dev Patel <dvptl68@gmail.com>
2020-07-05 09:14:24 -0400

    Remove PDO test

--- php/action.php
+++ php/action.php
@@ -20,14 +20,7 @@ $myfile = fopen("config.json", "r");
 $config = json_decode(fread($myfile,filesize("config.json")), true);
 fclose($myfile);
 
-// Test PDO connection
-try {
-  $pdo = new PDO("mysql:host=localhost;dbname=database", 'username', 'password');
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+
 
 // Redirect to success page
 header('Location: ../success.html');
